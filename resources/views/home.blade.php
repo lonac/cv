8841287ca3be5e3bf5e9635668d7e0593512d7e8
lonac <barakamalonao@yahoo.com>
2017-06-06 18:26:08 +0300

    modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,13 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <div class="col-md-6 col-md-offset-1">
+            <div class="panel panel-inverse">
+                <div class="panel-heading">MY CV</div>
+                    {{--check if i have the cv--}}
 
+
+                    {{--if i dont have cv then allow  me create ones--}}
                 <div class="panel-body">
                     You are logged in!
                 </div>
