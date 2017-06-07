3d200ed7023d0534d3e4497b91f1eacc7ba0106c
lonac <barakamalonao@yahoo.com>
2017-06-07 12:11:44 +0300

    modified:   app/Http/Controllers/HomeController.php modified:   resources/views/home.blade.php modified:   resources/views/layout/navbar.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,14 @@
 @extends('layout.master')
-@section('title','Home')
+@section('title', $user->name)
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-6 col-md-offset-1">
             <div class="panel panel-inverse">
-                <div class="panel-heading">MY CV</div>
-                    {{--check if i have the cv--}}
-
-
-                    {{--if i dont have cv then allow  me create ones--}}
+                <div class="panel-heading">CV-CREATOR</div>
                 <div class="panel-body">
-                    You are logged in!
+                    
                 </div>
             </div>
         </div>
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">DIAW</a>
+            <a class="navbar-brand" href="/">CV-CREATOR</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
