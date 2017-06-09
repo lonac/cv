86124106f692d9e201aa2a9ff78ff8687a17025e
lonac <barakamalonao@yahoo.com>
2017-06-09 10:19:52 +0300

    modified:   resources/views/skills/_form.blade.php

--- resources/views/skills/_form.blade.php
+++ resources/views/skills/_form.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-8 col-md-offset-2">
 <div class="panel panel-inverse">
   @if($sk->count()>0)
-  <div class="panel-heading"> <strong>Skills:</strong></div>
+  <div class="panel-heading"> <strong>Skills/Talents:</strong></div>
   <div class="panel-body">
       @foreach($sk as $skee)
         <strong>Skill:</strong>{{$skee->skillname}}</br>
