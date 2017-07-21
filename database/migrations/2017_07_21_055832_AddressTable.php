117632fd6a6d166e069813bddad604b52d9b8910
lonac <barakamalonao@yahoo.com>
2017-07-21 09:04:14 +0300

    modified:   database/migrations/2017_07_21_055832_AddressTable.php

--- database/migrations/2017_07_21_055832_AddressTable.php
+++ database/migrations/2017_07_21_055832_AddressTable.php
@@ -13,7 +13,11 @@ class AddressTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('addresses', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('address');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +27,6 @@ class AddressTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('addresses');
     }
 }
