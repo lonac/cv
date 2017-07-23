5930d9ac402c409d7c97664f450169c99bdc7580
lonac <barakamalonao@yahoo.com>
2017-07-23 19:52:21 +0300

    modified:   resources/views/address/_form.blade.php

--- resources/views/address/_form.blade.php
+++ resources/views/address/_form.blade.php
@@ -1,12 +1,16 @@
 <div class="col-md-8 col-md-offset-2">
-<div class="paneladdress panel-inverse">
+<div class="panel panel-inverse">
   @if($address->count()>0)
-        @foreach($address as $addresse)
-            <h3>
-              Address : {{ $addresse->address}}  <a href="{{url('address/'.$addresse->id.'/edit')}}">Edit</a>
-            </h3>
-        @endforeach
+  	<div class="panel-heading"><strong> MY ADDRESS:</strong></div>
+	  	<div class="panel-body">
+		        @foreach($address as $addresse)
+		            <h3>
+		              Address : {{ $addresse->address}}  <a href="{{url('address/'.$addresse->id.'/edit')}}">Edit</a>
+		            </h3>
+		        @endforeach
+	     </div>
+     	@else
+     		You have no address!
     @endif
   </div>  
-</div>
 </div>
