d88ca79a95b9cb101748a20e11bb3218268ea7a7
lonac <barakamalonao@yahoo.com>
2017-07-23 20:03:45 +0300

    modified:   resources/views/phonenumbers/_form.blade.php modified:   resources/views/phonenumbers/edit.blade.php modified:   resources/views/phonenumbers/show.blade.php

--- resources/views/phonenumbers/_form.blade.php
+++ resources/views/phonenumbers/_form.blade.php
@@ -1,12 +1,16 @@
 <div class="col-md-8 col-md-offset-2">
-<div class="panel panel-inverse">
-  @if($myphonenumbers->count()>0)
-        @foreach($myphonenumbers as $phonee)
-            <h3>
-                Phone Number : {{ $phonee->phonenumber}}  <a href="{{url('phonenumbers/'.$phonee->id.'/edit')}}">Edit</a>
-            </h3>
-        @endforeach
-    @endif
-  </div>  
-</div>
+	<div class="panel panel-inverse">
+	  @if($myphonenumbers->count()>0)
+	  <div class="panel-heading"><strong>MY PHONE NUMBERS:</strong></div>		
+		<div class="panel-body">
+	        @foreach($myphonenumbers as $phonee)
+	            <h3>
+	                Phone Number : {{ $phonee->phonenumber}}  <a href="{{url('phonenumbers/'.$phonee->id.'/edit')}}">Edit</a>
+	            </h3>
+	        @endforeach
+	       @else
+	       		No phone number registered!
+		    @endif
+		 </div>  
+	</div>
 </div>
--- resources/views/phonenumbers/show.blade.php
+++ resources/views/phonenumbers/show.blade.php
@@ -5,12 +5,14 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-md-8 col-md-offset-2">
         		@if (session('status'))
 				    <div class="alert alert-success">
 				        {{ session('status') }}
 				    </div>
 				@endif
-             @include('phonenumbers._form')
+                 @include('phonenumbers._form')
+            </div>
         </div>
 
     </div>
